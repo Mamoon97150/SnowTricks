5307bf86a6fefa3567d2a2d7fc1a9a7602340582
Djamina Marboeuf <djamina.marboeuf@gmail.com>
2021-07-14 12:41:36 -0400

    fixes#4

---
+++ app/src/Security/UserChecker.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Security;
+
+use App\Entity\User;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAccountStatusException;
+use Symfony\Component\Security\Core\User\UserCheckerInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class UserChecker implements UserCheckerInterface
+{
+    /**
+     * @inheritDoc
+     */
+    public function checkPreAuth(UserInterface $user)
+    {
+        if (!$user instanceof User) {
+            return;
+        }
+
+        if (!$user->isVerified()) {
+            // the message passed to this exception is meant to be displayed to the user
+            throw new CustomUserMessageAccountStatusException('Please verify your email !');
+        }
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function checkPostAuth(UserInterface $user)
+    {
+        if (!$user instanceof User) {
+            return;
+        }
+    }
+}
